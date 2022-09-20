1a2352f92f12d3883f97d05939d184a7d428a909
min0808 <min0808mini@gmail.com>
2022-09-20 09:17:24 +0900

    Update MemberMain.java

--- javaproject/FirstJava/src/chapter05/Exam/MemberMain.java
+++ javaproject/FirstJava/src/chapter05/Exam/MemberMain.java
@@ -10,7 +10,7 @@ public class MemberMain {
 		
 		
 		Member sp1 = new Member("박민", "01075471556", "패션", "4", "min0808mini@naver.com", "910808","서울시");
-		Member sp2 = new Member("민", "01075471554");
+		Member sp2 = new Member("박민", "01075471556");
 		
 		sp1.showInfo();
 		System.out.println("--------------------");
