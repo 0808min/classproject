72083b56e41b3fd9c0d77785d8ef485fbe9bc169
min0808 <min0808mini@gmail.com>
2022-09-20 12:59:58 +0900

    Singleton

--- javaproject/FirstJava/src/chapter05/modifier/PrivateTest.java
+++ javaproject/FirstJava/src/chapter05/modifier/PrivateTest.java
@@ -1,19 +1,18 @@
 package chapter05.modifier;
 
 public class PrivateTest {
-	
-	String name;
-	int age;
-	
+
+	private String name;
+	private int age;
+
 	public PrivateTest() {
 		this.name = "박민";
 		this.age = 32;
 	}
-	
+
 	public void showInfo() {
 		System.out.println("이름 : " + this.name);
 		System.out.println("나이 : " + this.age);
 	}
-	
 
 }
--- javaproject/FirstJava/src/chapter05/modifier/PrivateTestMain.java
+++ javaproject/FirstJava/src/chapter05/modifier/PrivateTestMain.java
@@ -3,14 +3,14 @@ package chapter05.modifier;
 public class PrivateTestMain {
 
 	public static void main(String[] args) {
-		
+
 		PrivateTest pt = new PrivateTest();
-		
-		System.out.println(pt.name);
-		System.out.println(pt.age);
-		pt.age = 300;
-		
-		System.out.println(pt.age);
+
+		pt.showInfo();
+
+//		System.out.println(pt.name);
+//		System.out.println(pt.age);
+//		pt.age = 30;
 
 	}
 
