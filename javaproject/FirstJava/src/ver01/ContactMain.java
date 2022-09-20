5452ad7020d7b06144ba02248a0d92ad9ea7a884
min0808 <min0808mini@gmail.com>
2022-09-20 17:41:09 +0900

    Update ContactMain.java

--- javaproject/FirstJava/src/ver01/ContactMain.java
+++ javaproject/FirstJava/src/ver01/ContactMain.java
@@ -20,27 +20,27 @@ public class ContactMain {
 
 		while (true) {
 
-			System.out.print("이름을 입력해 주세요 >> ");
+			System.out.print("이름을 입력 >> ");
 			data.setName(sc.nextLine());
 
-			System.out.print("전화번호를 입력해 주세요 >> ");
+			System.out.print("전화번호를 입력 >> ");
 			data.setPhoneNumber(sc.nextLine());
 
-			System.out.print("이메일을 입력해 주세요 >> ");
+			System.out.print("이메일을 입력 >> ");
 			data.setEmail(sc.nextLine());
 
-			System.out.print("주소를 입력해 주세요 >> ");
+			System.out.print("주소를 입력 >> ");
 			data.setAddress(sc.nextLine());
 
-			System.out.print("생일을 입력해 주세요 >> ");
+			System.out.print("생일을 입력 >> ");
 			data.setBirthday(sc.nextLine());
 
-			System.out.print("그룹을 입력해 주세요 >> ");
+			System.out.print("그룹을 입력 >> ");
 			data.setGroup(sc.nextLine());
 
 			data.tell();
 
-			System.out.print("계속 진행 1, 종료 0 입력해주세요 >>>");
+			System.out.print("계속 진행 1, 종료 0 입력 >>>");
 
 			if (Integer.parseInt(sc.nextLine()) == 0) {
 				sc.close();
