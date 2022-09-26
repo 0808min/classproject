e6d79512f5cac902e31bc8ac968af1329ea62e93
min0808 <min0808mini@gmail.com>
2022-09-26 13:00:18 +0900

    Abstract

--- javaproject/FirstJava/src/chapter08/AbstractClass.java
+++ javaproject/FirstJava/src/chapter08/AbstractClass.java
@@ -2,7 +2,7 @@ package chapter08;
 
 public abstract class AbstractClass {
 
-//	추상클래스 : 맴버중 추상메소드가 하나라도 존재하면 추상클래스
+//	추상클래스 : 맴버중 추상메소드가 하나라도 존재하면 추상클래스가 된다
 //	어브스트랙트 키워드로 추상클래스임을 정의
 //	추상클래스는 미완성된 클래스이므로 인스턴스 생성이 불가
 //	추상클래스의 정의 목적은
@@ -17,5 +17,9 @@ public abstract class AbstractClass {
 
 //	추상메소드 : 메소드의 정의만 하고 처리부는 완성하지 않은 메소드
 	abstract void abstractMethod();
+
+	void instanceMethod() {
+		abstractMethod();
+	}
 
 }
