c1387c10e22d4731bd1e9ed0a8bf8958302c8b8b
min0808 <min0808mini@gmail.com>
2022-09-26 15:09:07 +0900

    interface

--- javaproject/FirstJava/src/chapter08/test/PhoneImpl.java
+++ javaproject/FirstJava/src/chapter08/test/PhoneImpl.java
@@ -1,5 +1,13 @@
 package chapter08.test;
 
-public class PhoneImpl {
+import chapter08.NewClass;
+
+public class PhoneImpl extends NewClass implements Phone {
+	
+	public void print() {
+		
+		System.out.println("프린트 메소드를 구현했습니다.");
+		
+	}
 
 }
