e20689a73a8a438c3838fee310de016f8200b347
min0808 <min0808mini@gmail.com>
2022-09-26 14:03:07 +0900

    phone

--- javaproject/FirstJava/src/chapter08/Iphone.java
+++ javaproject/FirstJava/src/chapter08/Iphone.java
@@ -2,9 +2,15 @@ package chapter08;
 
 public class Iphone extends Phone {
 
+	public Iphone(String phoneNumber, boolean power) {
+		super(phoneNumber);
+
+	}
+
 	@Override
 	void turnOn() {
-		// TODO Auto-generated method stub
+		System.out.println("아이폰의 전원을 켭니다.");
+		power = true;
 
 	}
 
--- javaproject/FirstJava/src/chapter08/Phone.java
+++ javaproject/FirstJava/src/chapter08/Phone.java
@@ -1,9 +1,19 @@
 package chapter08;
 
-public class Phone {
+public abstract class Phone {
 
-	public Phone() {
-		// TODO Auto-generated constructor stub
+	String phoneNumber;
+	boolean power;
+
+	public Phone(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+		this.power = false;
 	}
 
+	void call() {
+		System.out.println("전화 걸기");
+	}
+
+//	추상 메소드로 정의
+	abstract void turnOn();
 }
