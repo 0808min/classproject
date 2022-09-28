3c678eccbbd6ab1fbb9edc4db8da5f59e2e7aa7f
min0808 <min0808mini@gmail.com>
2022-09-28 15:27:13 +0900

    A

--- javaproject/FirstJava/src/chapter10/ExcptionCatchTest.java
+++ javaproject/FirstJava/src/chapter10/ExcptionCatchTest.java
@@ -16,26 +16,25 @@ public class ExcptionCatchTest {
 
 		try {
 			System.out.println("num1 / num2 = " + num1 / num2); // <- 예외가 발생할 수 있는 위치
-			
 			String str = null;
-			
 			System.out.println(str.length());
-			
-			
+
 		} catch (ArithmeticException e) {
 			System.out.println("0으로 나누는것은 불가합니다.");
 			e.printStackTrace();
 			System.out.println(e.getMessage());
-		} catch(NullPointerException e) {
+			return;
+		} catch (NullPointerException e) {
 			System.out.println("null");
-		}
-		
-		catch (Exception e) {
+		} catch (Exception e) {
 			System.out.println("예외발생");
 			e.printStackTrace();
+		} finally {
+			System.out.println("무조건 실행하는 블럭입니다.");
+
+			
 		}
-		
-		System.out.println("프로그램을 종료합니다.");
+		System.out.println("프로그램을 처리 완료.");
 
 	}
 
