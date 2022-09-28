c532b1bd128d8b44a510dfde4a906d67f2dd8a9d
min0808 <min0808mini@gmail.com>
2022-09-28 12:00:16 +0900

    Update Person.java

--- javaproject/FirstJava/src/chapter09/exam/Person.java
+++ javaproject/FirstJava/src/chapter09/exam/Person.java
@@ -1,15 +1,18 @@
 package chapter09.exam;
 
-import chapter09.Car;
+import chapter09.Person;
 
 public class Person extends Object {
 
 //	1. 위 Person 클래스의 equals() 메소드를 오버라이딩해서 주민등록번호가 같으면 인스턴스로 판별하는 프로그램을 만들어봅시다. 
-
+	String name;
 	String personNumber;
 
-	Person(String personNumber) {
+	Person(String personNumber, String name) {
+		
 		this.personNumber = personNumber;
+		this.name = name;
+	
 	}
 
 	@Override
@@ -28,12 +31,19 @@ public class Person extends Object {
 
 	@Override
 	public String toString() {
-		return "Person [personNumber = " + personNumber + "]";
-
+		return "Person [personNumber=" + personNumber + "]";
+		
+//		2. 1~100,000,000까지 더하기하는 연산의 실행 시간을 측정하는 프로그램을 만들어봅시다
+		
+		
+		
+		
+		
 	}
 
 	public static void main(String[] args) {
-
+		
+		Person name = new Person("박민");
 		Person personNum1 = new Person("910808");
 		Person personNum2 = new Person("930705");
 
