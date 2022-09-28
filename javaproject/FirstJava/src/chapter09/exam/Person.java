0b997db479202546463b450ea98cc3f81f0dce2b
min0808 <min0808mini@gmail.com>
2022-09-28 12:32:51 +0900

    *

--- javaproject/FirstJava/src/chapter09/exam/Person.java
+++ javaproject/FirstJava/src/chapter09/exam/Person.java
@@ -10,7 +10,6 @@ public class Person {
 		this.name = name;
 		this.personNumber = personNumber;
 
-	
 	}
 
 	@Override
@@ -27,14 +26,12 @@ public class Person {
 		return result;
 	}
 
-		
-		
-		
-		
-		
+	public static void main(String[] args) {
 
-	
+		Person p1 = new Person("min", "910808");
+		Person p2 = new Person("jin", "930705");
+		System.out.println(p1.equals(p2));
 
-	
+	}
 
 }
