abe40fb9f3b32bd2865c122b992d7a948b3d549c
min0808 <min0808mini@gmail.com>
2022-09-29 17:44:53 +0900

    exam11

---
+++ javaproject/FirstJava/src/chapter11/exam/FootballPlayer.java
@@ -0,0 +1,75 @@
+package chapter11.exam;
+
+import java.util.Objects;
+
+public class FootballPlayer implements Comparable<FootballPlayer> {
+
+//	String name;
+//	 int number;
+//	 String team;
+//	 int age
+
+//	1. 축구선수 인스턴스를 저장할 수 있는 List<E> 컬렉션 인스턴스를 생성해서 인스턴스를 저장하고 출력하는 프로그램을 만들어 봅시다. 
+
+	private String name;
+	private int number;
+	private String team;
+	private int age;
+
+	public FootballPlayer(String name, int number, String team, int age) {
+
+		this.name = name;
+		this.number = number;
+		this.team = team;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(age, name, number, team);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FootballPlayer other = (FootballPlayer) obj;
+		return age == other.age && Objects.equals(name, other.name) && number == other.number
+				&& Objects.equals(team, other.team);
+	}
+
+	@Override
+	public String toString() {
+		return "FootballPlayer [name=" + name + ", number=" + number + ", team=" + team + ", age=" + age + "]";
+	}
+
+	@Override
+	public int compareTo(FootballPlayer f) {
+		int compare = this.name.compareTo(f.getName());
+		if (compare == 0) {
+			compare = Integer.compare(this.number, f.getNumber());
+		}
+		return compare;
+	}
+
+}
