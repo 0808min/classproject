21ca309b0e338837c8acc61130da6bb4123af40b
min0808 <min0808mini@gmail.com>
2022-09-30 11:12:05 +0900

    Exam11

--- javaproject/FirstJava/src/chapter11/exam/Exam3Main.java
+++ javaproject/FirstJava/src/chapter11/exam/Exam3Main.java
@@ -1,28 +1,26 @@
 package chapter11.exam;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.TreeSet;
 
-public class Exam2Main {
+public class Exam3Main {
 
 	public static void main(String[] args) {
-//		2. 축구선수의 인스턴스가 팀과 이름 그리고 나이가 같으면 같은 선수라 판단하고 
-//		입력이 되지 않도록 Set<E> 컬렉션을 이용해서 
-//		축구선수 인스턴스를 저장하고 출력하는 프로그램을 만들어 봅시다.
+//		3. TreeSet<E>을 이용해서 
+//		팀 이름순으로 정렬하고, 
+//		같은 팀의 선수들은 이름 순으로 정렬하고, 
+//		같은 이름의 선수는 번호 순으로 저장하는 프로그램을 만들어 봅시다.
+		
+		TreeSet<FootballPlayer2> players = new TreeSet<>();
+		
 
-//		Set<E> : 중복허용 안함, 저장 순서도 유지하지 않음.
-//		중복검사 : hashcode로 검사 -> equals를 통해 중복검사
-
-		Set<FootballPlayer2> players = new HashSet<FootballPlayer2>();
 		
 		FootballPlayer2 player = new FootballPlayer2("음바페", 7, "생제르멩", 24);
-		players.add(player);
 		players.add(player);
 		players.add(new FootballPlayer2("앙리", 10, "아스널", 25));
 		players.add(new FootballPlayer2("반 데이크", 4, "리버풀", 28));
 		players.add(new FootballPlayer2("살라", 7, "리버풀", 30));
 		players.add(new FootballPlayer2("루니", 10, "맨유", 35));
-		players.add(new FootballPlayer2("음바페", 7, "생제르멩", 24));
+		players.add(new FootballPlayer2("음바페", 17, "생제르멩", 24));
 
 //		List<E> : 저장 순서를 유지 -> index 값을 가지고, 일괄처리
 		System.out.println("보유 선수의 수 : " + players.size());
@@ -31,5 +29,7 @@ public class Exam2Main {
 		for (FootballPlayer2 p : players) {
 			System.out.println(p);
 		}
+
 	}
+
 }
