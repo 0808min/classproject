42ae4990baf4177008d78faa806c11760a39e0f9
min0808 <min0808mini@gmail.com>
2022-09-30 17:54:06 +0900

    Update TestWrapperClass.java

--- javaproject/FirstJava/src/Test/TestWrapperClass.java
+++ javaproject/FirstJava/src/Test/TestWrapperClass.java
@@ -1,9 +1,34 @@
 package Test;
 
+import java.util.ArrayList;
+
 public class TestWrapperClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		ArrayList<Integer> list = new ArrayList<>();
+
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		list.add(40);
+		list.add(50);
+		
+		printAll(list);
+		
+		ArrayList<String> list2 = new ArrayList<>();
+		list2.add("Min");
+		list2.add("Park");
+		
+		printAll(list2);
+
+	}
+
+	static <E> void printAll(ArrayList<E>list) {
+		
+		for(E obj : list) {
+			System.out.println(obj);
+		}
 
 	}
 
