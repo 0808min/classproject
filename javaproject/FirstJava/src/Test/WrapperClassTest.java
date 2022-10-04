be204c39c762c6872fc5ac95f4d8bf6751fe69dd
min0808 <min0808mini@gmail.com>
2022-10-04 17:39:06 +0900

    Test

--- javaproject/FirstJava/src/Test/WrapperClassTest.java
+++ javaproject/FirstJava/src/Test/WrapperClassTest.java
@@ -2,12 +2,12 @@ package Test;
 
 import java.util.ArrayList;
 
-public class TestWrapperClass {
-
+public class WrapperClassTest {
+	
 	public static void main(String[] args) {
-
+		
 		ArrayList<Integer> list = new ArrayList<>();
-
+		
 		list.add(10);
 		list.add(20);
 		list.add(30);
@@ -17,19 +17,20 @@ public class TestWrapperClass {
 		printAll(list);
 		
 		ArrayList<String> list2 = new ArrayList<>();
-		list2.add("Min");
-		list2.add("Park");
+		list2.add("박민");
+		list2.add("주경진");
 		
 		printAll(list2);
-
+		
 	}
-
-	static <E> void printAll(ArrayList<E>list) {
+	
+	static <E> void printAll(ArrayList<E> list) {
 		
 		for(E obj : list) {
 			System.out.println(obj);
 		}
-
+		
+		
 	}
 
 }
