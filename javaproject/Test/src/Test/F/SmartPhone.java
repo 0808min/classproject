dc5cf26563eba2ca1c0c5d7142cc1858fcab5cb9
min0808 <min0808mini@gmail.com>
2022-10-05 17:10:25 +0900

    Update SmartPhone.java

--- javaproject/Test/src/Test/F/SmartPhone.java
+++ javaproject/Test/src/Test/F/SmartPhone.java
@@ -1,6 +1,6 @@
 package Test.F;
 
-public class SmartPhone {
+public class SmartPhone implements Comparable<SmartPhone> {
 
 	private String name;
 	private String phoneNumber;
@@ -21,29 +21,33 @@ public class SmartPhone {
 
 	@Override
 	public int hashCode() {
-		return phoneNumber.charAt(phoneNumber.length()-1);
+		return phoneNumber.charAt(phoneNumber.length() - 1);
 //		0 ~ 9사이의 값이 나온다
-		
+
 	}
 
 	@Override
 	public boolean equals(Object obj) {
-		
+
 		boolean result = false;
-		
-		if(obj != null && obj instanceof SmartPhone) {
+
+		if (obj != null && obj instanceof SmartPhone) {
 			SmartPhone phone = (SmartPhone) obj;
 			result = phoneNumber.equals(phone.getPhoneNumber());
 		}
-		
-		
+
 		return result;
 	}
-	
+
+	@Override
+	public int compareTo(SmartPhone o) {
+		
+		return name.compareTo(o.getName());
+	}
+
 	@Override
 	public String toString() {
 		return "SmartPhone [name=" + name + ", phoneNumber=" + phoneNumber + "]";
 	}
-
 
 }
