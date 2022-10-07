5d912c6c0c763edfc7a769cf1b41dfa87ec7cda8
min0808 <min0808mini@gmail.com>
2022-10-07 11:18:46 +0900

    13

--- javaproject/FirstJava/src/chapter13/MyThread.java
+++ javaproject/FirstJava/src/chapter13/MyThread.java
@@ -5,8 +5,18 @@ public class MyThread extends Thread {
 
 	@Override
 	public void run() {
-		System.out.println("새로운 스레드가 생성되어 실행합니다.");
+		for (int i = 0; i < 1000; i++) {
+			System.out.println("Runnable 인터페이스를 이용한 스레드 생성과 작업이 진행됩니다.");
+//			try {
+//				Thread.sleep(500);
+//				System.out.println("새로운 스레드가 생성되어 실행합니다.");
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
 
+		}
+		
 	}
 
 }
