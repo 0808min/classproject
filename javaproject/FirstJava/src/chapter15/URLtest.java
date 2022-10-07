506abff3c59cfc24a8f7a949fc530a952dc7f454
min0808 <min0808mini@gmail.com>
2022-10-07 14:23:56 +0900

    Create URLtest.java

---
+++ javaproject/FirstJava/src/chapter15/URLtest.java
@@ -0,0 +1,31 @@
+package chapter15;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class URLtest {
+	
+	public static void main(String[] args) {
+		
+		String urlStr = "http://cafe.naver.com/main/read.nhn?mode=LS2D&mid=sec&sid1=105&sid2=228&oid=584&aid=";
+		
+		try {
+			
+			URL url = new URL(urlStr);
+			
+			System.out.println(url.getProtocol());
+			System.out.println(url.getHost());
+			System.out.println(url.getPort());
+			System.out.println(url.getDefaultPort());
+			System.out.println(url.getPath());
+			System.out.println(url.getQuery());
+			
+			
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
